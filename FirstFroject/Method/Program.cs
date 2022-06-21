dd93b052d9a76e149d87b26436c7118feb4b9aa7
jacejang1027 <jacejang1027@gmail.com>
2022-06-21 21:51:30 +0900

    1
    Operators
    OperatorMethods

---
+++ FirstFroject/Method/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OperatorMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine(OperatorMethods.Sum(1, 2));
+            }
+        }
+    }
+}
